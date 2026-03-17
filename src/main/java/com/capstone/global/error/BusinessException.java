dd8c2138d34f45d2bcf04939db3d760d9f254a40
88guri <quffl2002@naver.com>
2026-03-17 22:42:54 +0900

    feat : #6 생성자에서 null체크를 강제함 NPE 방지

--- src/main/java/com/capstone/global/error/BusinessException.java
+++ src/main/java/com/capstone/global/error/BusinessException.java
@@ -2,13 +2,15 @@ package com.capstone.global.error;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class BusinessException extends RuntimeException {
 
   private final ErrorStatus errorStatus;
 
   public BusinessException(ErrorStatus errorStatus) {
-    super(errorStatus.getMessage());
+    super(Objects.requireNonNull(errorStatus, "ErrorStatus must not be null").getMessage());
     this.errorStatus = errorStatus;
   }
 }
