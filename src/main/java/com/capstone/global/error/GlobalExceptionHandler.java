e209e3808de0658b3404a1ad20ed9f7253b5f8a9
88guri <quffl2002@naver.com>
2026-03-17 22:10:05 +0900

    feat : #6 전역 예외 핸들러 생성

---
+++ src/main/java/com/capstone/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.capstone.global.error;
+
+import com.capstone.global.common.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+        ApiResponse.fail(ErrorStatus.INTERNAL_SERVER_ERROR)
+    );
+  }
+
+  @ExceptionHandler(BusinessException.class)
+  public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
+
+    return ResponseEntity.status(e.getErrorStatus().getHttpStatus()).body(
+        ApiResponse.fail(e.getErrorStatus())
+    );
+  }
+}
