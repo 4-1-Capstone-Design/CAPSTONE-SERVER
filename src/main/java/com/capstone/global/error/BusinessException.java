aa69ef75360b44b57c024de015b0fa23bb136c1f
88guri <quffl2002@naver.com>
2026-03-17 22:11:10 +0900

    feat : #6 커스텀 예외 생성

---
+++ src/main/java/com/capstone/global/error/BusinessException.java
@@ -0,0 +1,14 @@
+package com.capstone.global.error;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final ErrorStatus errorStatus;
+
+  public BusinessException(ErrorStatus errorStatus) {
+    super(errorStatus.getMessage());
+    this.errorStatus = errorStatus;
+  }
+}
