c5b8649f6957ebe72f50e9a99550ceaf7167cd69
88guri <quffl2002@naver.com>
2026-03-31 17:47:34 +0900

    feat : #24 특정 일의 journal 조회

--- src/main/java/com/capstone/domain/journal/repository/JournalRepository.java
+++ src/main/java/com/capstone/domain/journal/repository/JournalRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface JournalRepository extends JpaRepository<Journal, Long> {
   boolean existsByUserIdAndJournalDateAndIsDeletedFalse(Long userId, LocalDate journalDate);
   Optional<Journal> findByIdAndIsDeletedFalse(Long journalId);
+  Optional<Journal> findByUserIdAndJournalDateAndIsDeletedFalse(Long userId, LocalDate journalDate);
 }
