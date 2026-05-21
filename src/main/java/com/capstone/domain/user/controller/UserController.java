78548af800f59969c3aec3f7d6a04184733425eb
88guri <quffl2002@naver.com>
2026-05-21 14:20:46 +0900

    feat: #43 마이페이지 API 구현

--- src/main/java/com/capstone/domain/user/controller/UserController.java
+++ src/main/java/com/capstone/domain/user/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.capstone.domain.user.controller;
 
+import com.capstone.domain.user.dto.response.MyPageResponseDto;
 import com.capstone.domain.user.service.UserService;
+import com.capstone.global.common.ApiResponse;
+import com.capstone.global.common.SuccessStatus;
+import com.capstone.global.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,4 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
   private final UserService userService;
+
+  @GetMapping("/me")
+  public ResponseEntity<ApiResponse<MyPageResponseDto>> getMyPage(
+      @AuthenticationPrincipal UserPrincipal userPrincipal
+  ) {
+    MyPageResponseDto response = userService.getMyPage(userPrincipal.getUserId());
+    return ResponseEntity.ok(ApiResponse.success(SuccessStatus.OK, response));
+  }
 }
