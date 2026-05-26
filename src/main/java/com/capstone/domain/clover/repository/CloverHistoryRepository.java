96235b2a1019f4cdb12caf29873d82c8947c0e5b
88guri <quffl2002@naver.com>
2026-05-26 17:10:59 +0900

    feat: #53 회원탈퇴용 레포지토리 및 요청 DTO 추가

---
+++ src/main/java/com/capstone/domain/clover/repository/CloverHistoryRepository.java
@@ -0,0 +1,14 @@
+package com.capstone.domain.clover.repository;
+
+import com.capstone.domain.clover.entity.CloverHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CloverHistoryRepository extends JpaRepository<CloverHistory, Long> {
+
+  @Modifying
+  @Query("delete from CloverHistory ch where ch.user.id = :userId")
+  void deleteByUserId(@Param("userId") Long userId);
+}
