46f2c1975250f08c15a9a6e13772b06c69c8088a
88guri <quffl2002@naver.com>
2026-05-26 17:11:26 +0900

    feat: #53 회원탈퇴 시 관련 데이터 벌크 삭제 쿼리 추가

--- src/main/java/com/capstone/domain/question/repository/QuestionAnswerKeywordRepository.java
+++ src/main/java/com/capstone/domain/question/repository/QuestionAnswerKeywordRepository.java
@@ -2,12 +2,24 @@ package com.capstone.domain.question.repository;
 
 import com.capstone.domain.question.entity.QuestionAnswerKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface QuestionAnswerKeywordRepository extends JpaRepository<QuestionAnswerKeyword, Long> {
+
+  @Modifying
+  @Query("""
+      delete from QuestionAnswerKeyword qak
+      where qak.questionAnswer.id in (
+          select qa.id from QuestionAnswer qa
+          join qa.dailyQuestion dq
+          where dq.user.id = :userId
+      )
+      """)
+  void deleteByUserId(@Param("userId") Long userId);
 
     @Query("""
             select qak.keyword.name, count(qak.id), sum(qak.score)
